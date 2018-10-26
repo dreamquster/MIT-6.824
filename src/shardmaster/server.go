0e723f80cdfec6b391ce69d5940980d1355a38fd
dreamquster <dreamquster>
2018-10-26 23:27:32 +0800

    shardmaster类似kvraft处理

--- src/shardmaster/server.go
+++ src/shardmaster/server.go
@@ -70,14 +70,98 @@ func (sm *ShardMaster) Join(args *JoinArgs, reply *JoinReply) {
 
 func (sm *ShardMaster) Leave(args *LeaveArgs, reply *LeaveReply) {
 	// Your code here.
+	index, _, isLeader := sm.rf.Start(Op{OpType:LEAVE, Args: *args})
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+
+	sm.mu.Lock()
+	if _, ok := sm.messages[index]; !ok {
+		sm.messages[index] = make(chan Result, 1)
+	}
+	chanMsg := sm.messages[index]
+	sm.mu.Unlock()
+
+	select {
+	case msg := <-chanMsg:
+		if recvArgs, ok := msg.args.(LeaveArgs); !ok {
+			reply.WrongLeader = true
+		} else {
+			if args.ClientId != recvArgs.ClientId || args.RequestId != recvArgs.RequestId {
+				reply.WrongLeader = true
+			} else {
+				*reply = msg.reply.(LeaveReply)
+				reply.WrongLeader = false
+			}
+		}
+	case <-time.After(1 * time.Second):
+		break
+	}
 }
 
 func (sm *ShardMaster) Move(args *MoveArgs, reply *MoveReply) {
 	// Your code here.
+	index, _, isLeader := sm.rf.Start(Op{OpType:MOVE, Args: *args})
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+
+	sm.mu.Lock()
+	if _, ok := sm.messages[index]; !ok {
+		sm.messages[index] = make(chan Result, 1)
+	}
+	chanMsg := sm.messages[index]
+	sm.mu.Unlock()
+
+	select {
+	case msg := <-chanMsg:
+		if recvArgs, ok := msg.args.(MoveArgs); !ok {
+			reply.WrongLeader = true
+		} else {
+			if args.ClientId != recvArgs.ClientId || args.RequestId != recvArgs.RequestId {
+				reply.WrongLeader = true
+			} else {
+				*reply = msg.reply.(MoveReply)
+				reply.WrongLeader = false
+			}
+		}
+	case <-time.After(1 * time.Second):
+		break
+	}
 }
 
 func (sm *ShardMaster) Query(args *QueryArgs, reply *QueryReply) {
 	// Your code here.
+	index, _, isLeader := sm.rf.Start(Op{OpType:QUERY, Args: *args})
+	if !isLeader {
+		reply.WrongLeader = true
+		return
+	}
+
+	sm.mu.Lock()
+	if _, ok := sm.messages[index]; !ok {
+		sm.messages[index] = make(chan Result, 1)
+	}
+	chanMsg := sm.messages[index]
+	sm.mu.Unlock()
+
+	select {
+	case msg := <-chanMsg:
+		if recvArgs, ok := msg.args.(QueryArgs); !ok {
+			reply.WrongLeader = true
+		} else {
+			if args.ClientId != recvArgs.ClientId || args.RequestId != recvArgs.RequestId {
+				reply.WrongLeader = true
+			} else {
+				*reply = msg.reply.(QueryReply)
+				reply.WrongLeader = false
+			}
+		}
+	case <-time.After(1 * time.Second):
+		break
+	}
 }
 
 
@@ -95,6 +179,44 @@ func (sm *ShardMaster) Kill() {
 // needed by shardkv tester
 func (sm *ShardMaster) Raft() *raft.Raft {
 	return sm.rf
+}
+
+func (sm *ShardMaster) DoUpdate()  {
+	for true {
+		applyMsg := <- sm.applyCh
+		if applyMsg.UseSnapshot {
+		} else {
+			request := applyMsg.Command.(Op)
+
+			var result Result
+			var clientId int64
+			var requestId int64
+			switch request.OpType {
+			case QUERY:
+				args := request.Args.(QueryArgs)
+				clientId = args.ClientId
+				requestId = args.RequestId
+			case JOIN:
+				args := request.Args.(JoinArgs)
+				clientId = args.ClientId
+				requestId = args.RequestId
+			case LEAVE:
+				args := request.Args.(LeaveArgs)
+				clientId = args.ClientId
+				requestId = args.RequestId
+			case MOVE:
+				args := request.Args.(MoveArgs)
+				clientId = args.ClientId
+				requestId = args.RequestId
+			}
+			sm.Apply(request, sm.IsDuplicate(clientId, requestId))
+		}
+
+	}
+}
+
+func (sm *ShardMaster) Apply(op Op, duplicate bool) interface{}   {
+
 }
 
 //
